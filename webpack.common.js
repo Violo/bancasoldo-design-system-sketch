af3b83e0fcb77180fe6e41c8395bd6b2f8ef1434
Alessandro <a.violini@e-xtrategy.net>
2019-03-05 15:23:12 +0100

    pattern per bancasoldo

---
+++ webpack.common.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: [path.resolve(__dirname, "src/index.js"), path.resolve(__dirname, "src/index.scss")],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            attrs:false
+                        }
+                    }
+                ]
+            },{
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {},
+                    },
+                ],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader", {
+                        loader: 'resolve-url-loader'
+                    },
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(["dist"]),
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+        }),
+        
+    ]
+};
